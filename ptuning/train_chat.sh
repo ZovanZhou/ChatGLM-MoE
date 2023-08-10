61366103ceeb1c7813083e3b09b0dac9e78ad631
zovan <59961985+ZovanZhou@users.noreply.github.com>
2023-08-10 16:06:12 +0800

    initial update

---
+++ ptuning/train_chat.sh
@@ -0,0 +1,31 @@
+PRE_SEQ_LEN=128
+PRE_N_EXPERTS=2
+PRE_CUR_TASK=1
+LR=1e-2
+
+CUDA_VISIBLE_DEVICES=1 python3 main.py \
+    --do_train \
+    --train_file ../child-emotional-code/data/chat_train_data.json \
+    --validation_file ../child-emotional-code/data/chat_train_data.json \
+    --prompt_column prompt \
+    --response_column response \
+    --history_column history \
+    --overwrite_cache \
+    --model_name_or_path ../base-model/chatglm-6b \
+    --output_dir ../child-emotional-code/checkpoints \
+    --overwrite_output_dir \
+    --max_source_length 256 \
+    --max_target_length 256 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 2 \
+    --predict_with_generate \
+    --max_steps 3000 \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate $LR \
+    --prefix_n_experts $PRE_N_EXPERTS \
+    --prefix_cur_expert $PRE_CUR_TASK \
+    --pre_seq_len $PRE_SEQ_LEN \
+    --quantization_bit 4
+
